012523682059be8288944af6ed84cd14a4d3047b
Amaresh Maharana <amareshcoder@gmail.com>
2025-12-18 10:01:29 +0530

    feat: enhance authentication with GitHub social provider and email OTP verification

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,38 @@
+import "server-only";
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins";
+
 import { prisma } from "./db";
+import { env } from "./env";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  socialProviders: { 
-    github: { 
-      clientId: "", 
-      clientSecret: ""
-    }, 
-  }, 
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        const { data, error } = await resend.emails.send({
+          from: "LearnMate <onboarding@resend.dev>",
+          to: [email],
+          subject: "LearnMate - Verify your email",
+          html: `
+            <p>
+              Hi, Welcome to LearnMate! We're excited to have you on board. Please use the following One-Time Password (OTP) to verify your email address:
+            </p>
+            <p>Your verification code is: <strong>${otp}</strong></p>
+          `,
+        });
+      },
+    }),
+  ],
 });
