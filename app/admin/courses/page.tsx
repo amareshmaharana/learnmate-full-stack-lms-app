66ffcc26f4c433d3b4a47e6e2f3a3d0c0e59f282
Amaresh Maharana <amareshcoder@gmail.com>
2025-12-18 09:51:18 +0530

    feat: enhance CoursesPage layout with structured components

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,7 +1,22 @@
+import { buttonVariants } from "@/components/ui/button";
+import { IconCirclePlus } from "@tabler/icons-react";
+import Link from "next/link";
+
 export default function CoursesPage() {
   return (
     <>
-      <div>I think, This is the Courses page.</div>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+
+        <Link className={buttonVariants()} href="/admin/courses/create">
+          <IconCirclePlus className="size-5" />
+          Create Course
+        </Link>
+      </div>
+
+      <div>
+        <h1>All Courses</h1>
+      </div>
     </>
   );
 }
