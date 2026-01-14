5810ea4508e51d1f1e2be2f4958bdca0a30031fb
Amaresh Maharana <amareshcoder@gmail.com>
2026-01-14 15:00:05 +0530

    feat: configure image remote patterns for external storage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: `learnmate-lms-pt.fly.storage.tigris.dev`,
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
