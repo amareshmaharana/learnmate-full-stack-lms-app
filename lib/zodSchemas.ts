40ff1cdddd3020e12a84113e2ce3fffbe5c474cc
Amaresh Maharana <amareshcoder@gmail.com>
2026-01-18 22:32:29 +0530

    feat: add lessonSchema for lesson validation and update chapterSchema export

--- lib/zodSchemas.ts
+++ lib/zodSchemas.ts
@@ -55,4 +55,27 @@ export const courseSchema = z.object({
   }),
 });
 
+export const chapterSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  courseId: z.string().uuid({ message: "Invalid course ID" }),
+});
+
+export const lessonSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  chapterId: z.string().uuid({ message: "Invalid chapter ID" }),
+  courseId: z.string().uuid({ message: "Invalid course ID" }),
+  description: z
+    .string()
+    .min(3, { message: "Description must be at least 3 characters long" })
+    .optional(),
+  videoKey: z.string().optional(),
+  thumbnailKey: z.string().optional(),
+});
+
 export type CourseSchemaType = z.infer<typeof courseSchema>;
+export type ChapterSchemaType = z.infer<typeof chapterSchema>;
+export type LessonSchemaType = z.infer<typeof lessonSchema>;
