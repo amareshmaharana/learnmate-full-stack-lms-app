bdf7bf2fc031e164ca4b812fed8e6d5b4ad8af94
Amaresh Maharana <amareshcoder@gmail.com>
2026-01-20 22:51:48 +0530

    fix: add bottom margin to main container in LayoutPublic component

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -10,7 +10,7 @@ export default function LayoutPublic({
     <>
       <div>
         <Navbar />
-        <main className="container mx-auto px-4 md:px-6 lg:px-8">
+        <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-30">
           {children}
         </main>
       </div>
