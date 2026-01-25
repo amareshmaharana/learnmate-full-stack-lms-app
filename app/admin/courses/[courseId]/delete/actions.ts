f9c1aee1d717c4c82ad2640bd75ad2b0f72f9c32
Amaresh Maharana <amareshcoder@gmail.com>
2026-01-25 09:32:03 +0530

    feat: implement rate limiting and bot detection in deleteCourse action

--- app/admin/courses/[courseId]/delete/actions.ts
+++ app/admin/courses/[courseId]/delete/actions.ts
@@ -5,11 +5,40 @@ import { revalidatePath } from "next/cache";
 import { requireAdmin } from "@/app/data/admin/require-admin";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
+import arcjet, { fixedWindow } from "@/lib/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet.withRule(
+  fixedWindow({
+    mode: "LIVE",
+    window: "1m",
+    max: 5,
+  }),
+);
 
 export async function deleteCourse(courseId: string): Promise<ApiResponse> {
-  await requireAdmin();
+  const session = await requireAdmin();
 
   try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session?.user.id,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to rate limiting.",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "You are a bot!, if this is a mistake, contact our support.",
+        };
+      }
+    }
+
     await prisma.course.delete({
       where: {
         id: courseId,
