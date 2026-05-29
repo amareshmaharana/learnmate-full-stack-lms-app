b24a5befd8c79d78317e3ae3f704fffb1243f555
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-29 16:01:35 +0530

    feat: update SectionCards component to display dynamic dashboard statistics

--- components/sidebar/section-cards.tsx
+++ components/sidebar/section-cards.tsx
@@ -1,102 +1,94 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
+import {
+  IconBook2,
+  IconPlaylistX,
+  IconUsers,
+  IconUsersGroup,
+} from "@tabler/icons-react";
 
-import { Badge } from "@/components/ui/badge"
 import {
   Card,
-  CardAction,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+import { adminGetDashboardStats } from "@/app/data/admin/admin-get-dashboard-stats";
 
-export function SectionCards() {
+export async function SectionCards() {
+  const { totalUsers, totalLearners, totalCourses, totalLessons } =
+    await adminGetDashboardStats();
+
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
+        <CardHeader className="flex flex-row items-start justify-between gap-2">
+          <div>
+            <CardDescription>Total Users</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {totalUsers.toLocaleString()}
+            </CardTitle>
+          </div>
+          <IconUsers className="size-6 text-muted-foreground" />
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
           <div className="text-muted-foreground">
-            Visitors for the last 6 months
+            no. of registered users in LearnMate.
           </div>
         </CardFooter>
       </Card>
+
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingDown />
-              -20%
-            </Badge>
-          </CardAction>
+        <CardHeader className="flex flex-row items-start justify-between gap-2">
+          <div>
+            <CardDescription>Total Learners</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {totalLearners.toLocaleString()}
+            </CardTitle>
+          </div>
+          <IconUsersGroup className="size-6 text-muted-foreground" />
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
-          </div>
           <div className="text-muted-foreground">
-            Acquisition needs attention
+            no. of users who have taken at least one course.
           </div>
         </CardFooter>
       </Card>
+
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
+        <CardHeader className="flex flex-row items-start justify-between gap-2">
+          <div>
+            <CardDescription>Total Courses</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {totalCourses.toLocaleString()}
+            </CardTitle>
+          </div>
+          <IconBook2 className="size-6 text-muted-foreground" />
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
+          <div className="text-muted-foreground">
+            no. of courses created by instructors on LearnMate. Which are
+            available.
           </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
         </CardFooter>
       </Card>
+
       <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
-          </CardAction>
+        <CardHeader className="flex flex-row items-start justify-between gap-2">
+          <div>
+            <CardDescription>Total Lessons</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {totalLessons.toLocaleString()}
+            </CardTitle>
+          </div>
+          <IconPlaylistX className="size-6 text-muted-foreground" />
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
+          <div className="text-muted-foreground">
+            no. of lessons created by instructors on LearnMate. Which are
+            available.
           </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
         </CardFooter>
       </Card>
     </div>
-  )
+  );
 }
