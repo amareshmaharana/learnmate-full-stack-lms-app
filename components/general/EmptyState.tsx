7ce5d281474918fc215c344906b1b6fcbb9d39c8
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-30 22:08:06 +0530

    refactor: improve layout and formatting in EmptyState component

--- components/general/EmptyState.tsx
+++ components/general/EmptyState.tsx
@@ -9,19 +9,22 @@ interface iAppProps {
   href: string;
 }
 
-export function EmptyState({ buttonText, description, title, href }: iAppProps) {
+export function EmptyState({
+  buttonText,
+  description,
+  title,
+  href,
+}: iAppProps) {
   return (
     <>
-      <div className="flex flex-col flex-1 h-full items-center justify-center rounded-md border-dashed border p-8 text-center animate-in fade-in-50">
+      <div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-center gap-4 rounded-md border border-dashed p-10 text-center animate-in fade-in-50">
         <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
           <Ban className="size-10 text-red-600" />
         </div>
 
-        <h2 className="mt-6 text-3xl font-semibold">{title}</h2>
+        <h2 className="text-3xl font-semibold">{title}</h2>
 
-        <p className="mb-8 mt-2 text-center text-sm text-muted-foreground">
-          {description}
-        </p>
+        <p className="max-w-2xl text-sm text-muted-foreground">{description}</p>
 
         <Link href={href} className={buttonVariants()}>
           <PlusCircleIcon className="size-5 mr-2" />
