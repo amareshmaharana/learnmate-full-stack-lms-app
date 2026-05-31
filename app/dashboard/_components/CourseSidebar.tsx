9c50db49f06797caba2d503bda4df4f5073652bf
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-31 11:55:49 +0530

    feat: implement CourseSidebar component for displaying course details and lesson progress

---
+++ app/dashboard/_components/CourseSidebar.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { CourseSidebarDataType } from "@/app/data/course/get-course-sidebar-data";
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { Progress } from "@/components/ui/progress";
+import { ChevronDown, Play } from "lucide-react";
+import LessonItem from "./LessonItem";
+import { usePathname } from "next/navigation";
+
+interface iAppProps {
+  course: CourseSidebarDataType["course"];
+}
+
+export const CourseSidebar = ({ course }: iAppProps) => {
+  const pathname = usePathname();
+
+  const currentLessonId = pathname.split("/").pop() ?? "";
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="pb-4 pr-4 border-b border-border">
+        <div className="flex items-center gap-3 mb-3">
+          <div className="size-10 rounded-lg bg-primary/10 flex items-center justify-center shrink-0">
+            <Play className="size-5 text-primary" />
+          </div>
+
+          <div className="flex-1 min-w-0">
+            <h1 className="font-semibold text-base leading-tight truncate">
+              {course.title}
+            </h1>
+            <p className="text-sm text-muted-foreground truncate">
+              {course.category}
+            </p>
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <div className="flex justify-between text-sm">
+            <span className="text-muted-foreground">Progress</span>
+            <span className="font-medium">4 / 10 Lessons</span>
+          </div>
+          <Progress value={40} className="h-1.5" />
+          <p className="text-sm text-muted-foreground">40% Complete</p>
+        </div>
+      </div>
+
+      <div className="py-4 pr-4 space-y-2">
+        {course.chapter.map((chapter, index) => (
+          <Collapsible key={chapter.id} defaultOpen={index === 0}>
+            <CollapsibleTrigger asChild>
+              <Button
+                variant="outline"
+                className="w-full p-3 h-auto flex items-center gap-2"
+              >
+                <div className="shrink-0">
+                  <ChevronDown className="size-4 text-primary" />
+                </div>
+
+                <div className="flex-1 text-left min-w-0">
+                  <p className="font-semibold text-sm truncate text-foreground">
+                    {chapter.position}: {chapter.title}
+                  </p>
+
+                  <p className="text-sm text-muted-foreground truncate font-medium">
+                    {chapter.lessons.length} Lessons
+                  </p>
+                </div>
+              </Button>
+            </CollapsibleTrigger>
+
+            <CollapsibleContent className="mt-3 pl-6 border-l-2 space-y-3">
+              {chapter.lessons.map((lesson) => (
+                <LessonItem
+                  key={lesson.id}
+                  lesson={lesson}
+                  slug={course.slug}
+                  isActive={lesson.id === currentLessonId}
+                />
+              ))}
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
+      </div>
+    </div>
+  );
+};
