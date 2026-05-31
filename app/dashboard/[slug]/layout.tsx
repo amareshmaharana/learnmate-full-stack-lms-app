736046dafe1bd39267e376fd97af834921f033be
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-31 11:54:29 +0530

    feat: add CourseLayout component for course sidebar and main content display

---
+++ app/dashboard/[slug]/layout.tsx
@@ -0,0 +1,27 @@
+import { getCourseSidebarData } from "@/app/data/course/get-course-sidebar-data";
+import { CourseSidebar } from "../_components/CourseSidebar";
+
+interface iAppProps {
+  params: Promise<{ slug: string }>;
+  children: React.ReactNode;
+}
+
+export default async function CourseLayout({ children, params }: iAppProps) {
+  const { slug } = await params;
+
+  const course = await getCourseSidebarData(slug);
+
+  return (
+    <>
+      <div className="flex flex-1">
+        {/* sidebar - 30% */}
+        <div className="w-80 border-r border-border shrink-0">
+          <CourseSidebar course={course.course} />
+        </div>
+
+        {/* main content - 70% */}
+        <div className="flex-1 p-6 overflow-hidden">{children}</div>
+      </div>
+    </>
+  );
+}
