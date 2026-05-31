aab9f1e19087d552c8ed999b3774c9e860553d68
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-31 11:54:48 +0530

    feat: add CourseSlugRoute component for displaying course details

---
+++ app/dashboard/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export default function CourseSlugRoute() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Course Details</h1>
+      <p className="text-muted-foreground">
+        This is the course details page. Here you can view the course content
+        and start learning!
+      </p>
+    </>
+  );
+}
