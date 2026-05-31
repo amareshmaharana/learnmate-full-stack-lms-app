897043573ffcb8e405ca37be4023ea6ad9238d50
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-31 15:41:52 +0530

    feat: implement lesson completion button with transition and feedback

--- app/dashboard/[slug]/[lessonId]/_components/CourseContent.tsx
+++ app/dashboard/[slug]/[lessonId]/_components/CourseContent.tsx
@@ -1,14 +1,25 @@
+"use client";
+
+import { CheckCircle, VideoOff } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
 import { LessonContentType } from "@/app/data/course/get-lesson-content";
 import { RenderDescription } from "@/components/rich-text-editor/RenderDescription";
 import { Button } from "@/components/ui/button";
+import { tryCatch } from "@/hooks/try-catch";
 import { useConstructUrl } from "@/hooks/use-construct-url";
-import { CheckCircle, VideoOff } from "lucide-react";
+import { markLessonCompletedAction } from "../actions";
+import { useConfetti } from "@/hooks/use-confetti";
 
 interface iAppProps {
   data: LessonContentType;
 }
 
 export default function CourseContent({ data }: iAppProps) {
+  const [pending, startTransition] = useTransition();
+  const { triggerConfetti } = useConfetti();
+
   function VideoPlayer({
     thumbnailKey,
     videoKey,
@@ -41,6 +52,26 @@ export default function CourseContent({ data }: iAppProps) {
     );
   }
 
+  function onSubmit() {
+    startTransition(async () => {
+      const { data: result, error } = await tryCatch(
+        markLessonCompletedAction(data.id, data.chapter.course.slug),
+      );
+
+      if (error) {
+        toast.error("An unexpected error occurred. Please try again.");
+        return;
+      }
+
+      if (result.status === "success") {
+        toast.success(result.message);
+        triggerConfetti();
+      } else if (result.status === "error") {
+        toast.error(result.message);
+      }
+    });
+  }
+
   return (
     <div className="flex flex-col h-full bg-background pl-6">
       <VideoPlayer
@@ -49,15 +80,30 @@ export default function CourseContent({ data }: iAppProps) {
       />
 
       <div className="py-4 border-b">
-        <Button variant="outline" size="sm">
-          <CheckCircle className="size-4 mr-2 text-green-700" />
-          Mark as Completed
-        </Button>
+        {data.lessonProgress.length > 0 ? (
+          <Button
+            variant="outline"
+            className="bg-green-500/10 text-green-700 border-green-300 cursor-default hover:bg-green-500/10"
+          >
+            <CheckCircle className="size-4 text-green-700" />
+            <p className="text-sm text-muted-foreground">Lesson completed</p>
+          </Button>
+        ) : (
+          <Button
+            variant="outline"
+            className="w-auto cursor-pointer"
+            onClick={onSubmit}
+            disabled={pending}
+          >
+            <CheckCircle className="size-4 mr-2 text-green-700" />
+            Mark as Completed
+          </Button>
+        )}
       </div>
 
       <div className="space-y-3 pt-3">
         <h2 className="text-3xl font-bold tracking-tight text-foreground">
-          {data.title} 
+          {data.title}
         </h2>
         {data.description && (
           <RenderDescription json={JSON.parse(data.description)} />
